8a14db67c920cc0a5ec7147097e468cbd86a4b71
Halfarne <adam.mihula@seznam.cz>
2023-02-10 17:22:06 +0100

    config update and dwindle layout

--- config.h
+++ config.h
@@ -1,14 +1,15 @@
+#include "fibonacci.c"
 /* appearance */
 static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 2;  /* border pixel of windows */
 static const float rootcolor[]             = {0.3, 0.3, 0.3, 1.0};
 static const float bordercolor[]           = {0.5, 0.5, 0.5, 1.0};
-static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
+static const float focuscolor[]            = {0.588, 0.054, 0.125, 1.0};
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
 
-;/* tagging */
+/* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -22,9 +23,9 @@ static const Rule rules[] = {
 /* layout(s) */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[D]",      dwindle },
+	{ "[M]",      monocle },    
+	{ "[F]",      NULL }, /* means floating*/
 };
 
 /* monitors */
@@ -92,7 +93,7 @@ LIBINPUT_CONFIG_TAP_MAP_LMR -- 1/2/3 finger tap maps to left/middle/right
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
 /* If you want to use the windows key for MODKEY, use WLR_MODIFIER_LOGO */
-#define MODKEY WLR_MODIFIER_ALT
+#define MODKEY WLR_MODIFIER_LOGO
 
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
@@ -104,27 +105,24 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "kitty", NULL };
-static const char *ff[] = { "firefox", NULL };
-static const char *rofi[] = { "rofi -show drun", NULL };
+static const char *term[] = { "kitty", NULL };
+istatic const char *menu[] = { "rofi -show drun", NULL };
+istatic const char *browser[] = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_a,          spawn,          {.v = termcmd} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_f,		 spawn,          {.v = ff} },
-	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_k,          killclient,     {0} },
-	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                    XKB_KEY_space,      spawn,          {.v = rofi} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_v,		 togglefloating, {0} },
-	{ MODKEY,                    XKB_KEY_l,		 togglefullscreen,{0} },
-	{ MODKEY|WLR_MODIFIER_ALT,   XKB_KEY_q,		 quit,           {0} },
+	{ MODKEY,                    XKB_KEY_space,	 spawn,          {.v = menu} },
+	{ MODKEY,		     XKB_KEY_a,		 spawn,          {.v = term} },
+	{ MODKEY,		     XKB_KEY_f,		 spawn,          {.v = browser} },
+
+	{ MODKEY,		     XKB_KEY_k,          killclient,     {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_v,          setlayout,      {.v = &layouts[0]} },
+
+	{ MODKEY,		     XKB_KEY_v,		 togglefloating, {0} },
+	{ MODKEY,                    XKB_KEY_l,          togglefullscreen, {0} },
+
+	{ MODKEY|WLR_MODIFIER_ALT,   XKB_KEY_q,          quit,           {0} },
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
